c29a565d87e9f701bbda3bf214b231edc9858508
bbouley <b.bouley@hotmail.co.uk>
2015-12-23 17:20:58 -0700

    added in todo file

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ var badgeLink = returnSVGLink(badgeColour(getPct()), getPct());
 
 var download = function(uri, filename, callback) {
   request.head(uri, function(err, res, body) {
-    console.log('content-type:', res.headers['content-type']);
-
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
 };
---
+++ todo.md
@@ -0,0 +1,7 @@
+##To Do
+
+1. convert to module.exports - correct format for npm install
+1. write tests with mocha/chai
+1. tests will have to be written to mock out readfilesync function
+1. opts file to allow different pcts to match different colours
+1. check options on where to place coverage.png file
