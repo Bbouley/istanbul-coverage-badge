8bb6554b20aa057027dfb2192a2d43eb813b8026
bbouley <b.bouley@hotmail.co.uk>
2015-12-23 17:12:55 -0700

    uploaded initial copy from project

---
+++ index.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var http = require('http');
+var request = require('request');
+
+function getPct () {
+  var object = JSON.parse(fs.readFileSync('./coverage/coverage-summary.json', 'utf8'));
+  return object.total.lines.pct;
+}
+
+function badgeColour (pct) {
+  if (pct >= 80) {
+    return 'green';
+  } else if (pct < 80  && pct >= 60) {
+    return 'yellow';
+  } else {
+    return 'red';
+  }
+}
+
+function returnSVGLink (colour, pct) {
+  return 'https://img.shields.io/badge/Coverage-' + pct + '-' + colour + '.png';
+}
+
+var badgeLink = returnSVGLink(badgeColour(getPct()), getPct());
+
+var download = function(uri, filename, callback) {
+  request.head(uri, function(err, res, body) {
+    console.log('content-type:', res.headers['content-type']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+download(badgeLink, 'coverage.png', function() {
+  console.log(badgeLink);
+  console.log('done');
+});
